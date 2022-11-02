0c4be28fc515f3cbb2a14ab633a151b95fd72373
Mariusz Perzynski <m.perzyk@interia.pl>
2022-11-02 19:19:46 +0100

    guidehub profile

--- turex.py
+++ turex.py
@@ -19,3 +19,27 @@ t.goto(-150,200) # move
 
 t.home() # back to home 0,0
 
+t.clear() # clear the space
+
+t.color('white') # turtle color line
+
+turtle.bgcolor('black') # background color
+
+t.pensize(5) # increase the color line
+
+
+t.penup()
+t.goto(300,300) # move turtle to choice space / up (x,y)
+
+t.pendown()
+t.goto(-300,300) # move turtle to choice space / down (x,y)
+
+t.write('HEJECZKA',font=('Roboto',30,"bold")) # write-space with font roboto size 30px and bold letters
+
+# t.hideturtle() hide 
+# t.showturtle() show
+
+turtle.exitonclick() # exit with click from the window
+
+t.begin_fill() # before shape the figures
+t.end_fill() # after shape the figures
