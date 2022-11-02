1cb77c3b27dce0431c71c4a4ae68d7ff6c937b76
Mariusz Perzynski <m.perzyk@interia.pl>
2022-11-02 19:07:49 +0100

    3 - ex figures

---
+++ turex.py
@@ -0,0 +1,21 @@
+import turtle  
+
+t = turtle.Turtle()
+t.shape('turtle')
+
+# square
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+t.circle(100) # pi
+
+t.goto(150,200)
+t.goto(-150,200) # move 
+
+t.home() # back to home 0,0
+
